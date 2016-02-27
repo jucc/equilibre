a55badaf081c319af551e9d9a01038fb6ef6076d
Juliana Correa <jucorrea.mail@gmail.com>
2016-02-27 17:20:43 -0300

    better solution to broken glyphicons problem and dist structure with grunt copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,27 +2,36 @@ module.exports = function(grunt) {
 
 	// CONFIG TASKS
 
+	var scriptsList = ['bower_components/jquery/dist/jquery.min.js',
+										 'bower_components/bootstrap/dist/js/bootstrap.min.js',
+										 'js/*.js'];
+
+	var stylesList  = ['bootstrap-custom/css/bootstrap.min.css',
+	                   'bootstrap-custom/css/bootstrap-theme.min.css',
+										 'app/css/custombootstrap.css'];
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
 		// TASK: UGLIFY --------------
 
 		uglify: {
-			dist: { 				  // concatenate all scripts and minify them
-				src: ['bower_components/jquery/dist/jquery.min.js', 'bower_components/bootstrap/dist/js/bootstrap.min.js', 'js/*.js'],
-				dest: 'dist/js/scripts.min.js'
+			dist: { 				   // concatenate all scripts and minify them
+				src: scriptsList,
+				dest: 'dist/js/basescripts.min.js'
 			},
-			dev: {
-				options: {			// concatenate all scripts but keep them readable
+			dev: {					   // concatenate all scripts but keep them readable
+				options: {
 					beautify: true,
 					mangle: false,
 					compress: false,
 					preserveComments: 'all'
 				},
-				src: ['bower_components/jquery/dist/jquery.min.js', 'bower_components/bootstrap/dist/js/bootstrap.min.js', 'js/*.js'],
+				src: scriptsList,
 				dest:'dist/js/basescripts.min.js'
 			}
 		},
+
 
 		// TASK: CSSMIN
 
@@ -33,17 +42,22 @@ module.exports = function(grunt) {
   		},
   		target: {
     		files: {
-      		'dist/css/basecss.min.css': ['bower_components/bootstrap/dist/css/bootstrap.min.css', 'bower_components/bootstrap/dist/css/bootstrap-theme.min.css', 'app/css/custombootstrap.css']
+      		'dist/css/basecss.min.css': stylesList
     		}
   		}
 		},
+
 
 		// TASK: WATCH -------------------
 
 		watch: {
 			js: {					// rebuild dev script when js files change
-				files: ['js/*.js'],
+				files: ['app/js/*.js'],
 				tasks: ['uglify:dev']
+			},
+			html: {
+				files: ['app/*.html', 'app/includes/*.html'],
+				tasks: ['bake:includes']
 			}
 		},
 
@@ -62,18 +76,49 @@ module.exports = function(grunt) {
           "dist/rooms.html": "app/rooms.html",
         }
       },
-    }
+    },
+
+
+		// TASK : COPY -------------------
+
+		copy: {
+		  main: {
+				files: [
+		      // bootstrap glyphicon fonts
+		      {
+						expand: true,
+						flatten: true,
+						filter: 'isFile',
+    				cwd: 'bootstrap-custom/fonts/',
+    			 	src: '**',
+    			 	dest: 'dist/fonts/'
+					},
+					// images
+					{
+						expand: true,
+						flatten: true,
+						filter: 'isFile',
+    				cwd: 'app/img/',
+    			 	src: '**',
+    			 	dest: 'dist/img/'
+					}
+				]
+			}
+		}
+
 	});
+
 
 	// LOAD PLUGINS
 
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-bake' );
-	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-bake');
 
 	// REGISTER TASKS
 
-	grunt.registerTask('default', ['cssmin', 'bake:includes', 'uglify:dev']); // most used task = just type grunt
-	grunt.registerTask('dist',    ['cssmin', 'bake:includes', 'uglify:dist']);
+	grunt.registerTask('default', ['copy:main', 'cssmin', 'bake:includes', 'uglify:dev']); // most used task = just type grunt
+	grunt.registerTask('dist',    ['copy:main', 'cssmin', 'bake:includes', 'uglify:dist']);
 };
